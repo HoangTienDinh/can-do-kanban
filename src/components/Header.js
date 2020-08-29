abba4c81fb9b5b66283c4ef5b14a946ba561485c
Hoang Dinh <hoangdinh90@gmail.com>
2020-08-29 00:32:01 -0700

    add checkbox icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import { Toolbar, Typography } from '@material-ui/core'
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import { Toolbar, Typography } from "@material-ui/core";
+import CheckBoxIcon from "@material-ui/icons/CheckBox";
 
 const Header = () => {
-    return (
-        <AppBar position="relative">
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            Can-Do Kanban Board
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    )
-}
+  return (
+    <AppBar position="relative">
+      <Toolbar>
+        <CheckBoxIcon />
+        <Typography variant="h6" color="inherit" noWrap>
+          Can-Do Kanban Board
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-export default Header
+export default Header;
