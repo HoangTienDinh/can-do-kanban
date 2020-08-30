99d083bdcb815994a8497432daf1cd43a9864229
Hoang Dinh <hoangdinh90@gmail.com>
2020-08-29 17:59:35 -0700

    background color

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import Card from './Card'
+import React from "react";
+import Card from "./Card";
+import Container from "@material-ui/core/Container";
 
 // Each Column will have a title
 // A scrollable body of cards
 // AddCard comp button
 const Column = () => {
-    return (
-        <div>
-            I am going to be a column
-            <Card />
-        </div>
-    )
-}
+  return (
+    <Container maxWidth='xs' style={{ border: '1px solid'}}>
+      I am going to be a column
+      <Card />
+    </Container>
+  );
+};
 
-export default Column
+export default Column;
